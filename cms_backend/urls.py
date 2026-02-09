e9d2360a07280896a433e4c6e96309e824d150c5
Ajaya Kumar <ajayakumar4741@gmail.com>
2026-02-09 21:16:29 +0530

    moded

--- cms_backend/urls.py
+++ cms_backend/urls.py
@@ -16,4 +16,4 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
-urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
