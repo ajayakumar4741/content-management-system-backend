46e059b66316fc8dea2713c178427bf553584ee3
Ajaya Kumar <ajayakumar4741@gmail.com>
2026-09-07 17:45:41 +0530

    add to top

--- cms_backend/settings.py
+++ cms_backend/settings.py
@@ -86,9 +86,9 @@ CACHES = {
 }
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',    
-    'django.contrib.sessions.middleware.SessionMiddleware',
     "corsheaders.middleware.CorsMiddleware",
+    'django.middleware.security.SecurityMiddleware',    
+    'django.contrib.sessions.middleware.SessionMiddleware',    
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
